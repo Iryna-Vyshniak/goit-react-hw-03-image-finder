0d3c8a2d01d9d6e143fcfbdd59fc8be743d55999
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-05 11:55:23 +0300

    add GlobalStyle, theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,13 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { GlobalStyle } from './GlobalStyle';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        App
+        <GlobalStyle />
+      </div>
+    );
+  }
+}

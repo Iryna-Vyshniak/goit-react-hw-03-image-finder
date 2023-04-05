d30385d3011595b9170b73ec8a14faafebb2ffe6
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-05 13:26:42 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container } from './Layout.styled';
 import PropTypes from 'prop-types';
+import { Searchbar } from '../Searchbar/Searchbar';
 
 export const Layout = ({ children }) => {
   return (
     <Container>
+      <Searchbar />
       <main>{children}</main>
     </Container>
   );

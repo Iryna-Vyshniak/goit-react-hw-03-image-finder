60c27b267b5ff303474b7f28ce5a8f68383815bb
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-05 16:06:05 +0300

    add Modal component, add test button to open modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,40 @@
 import React, { Component } from 'react';
 import { GlobalStyle } from './GlobalStyle';
 import { Layout } from './Layout/Layout';
+import Modal from './Modal/Modal';
 
 export default class App extends Component {
+  state = {
+    isShowModal: false,
+  };
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  // custom methods
+
+  // for toggle modal
+  toggleModal = () => {
+    this.setState(({ isShowModal }) => ({ isShowModal: !isShowModal }));
+  };
+
   render() {
+    const { isShowModal } = this.state;
+
     return (
       <Layout>
+        {/* test button */}
+        <button type="button" onClick={this.toggleModal}>
+          Test Open Modal
+        </button>
+
+        {/* render modal by condition */}
+        {isShowModal && (
+          <Modal onClose={this.toggleModal}>
+            <img src="" alt="" />
+          </Modal>
+        )}
         <GlobalStyle />
       </Layout>
     );

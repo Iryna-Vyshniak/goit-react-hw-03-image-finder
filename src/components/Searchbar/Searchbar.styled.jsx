b2f0a72e0d3ff14c9b2c301245a7871d82aa8f92
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-06 14:11:00 +0300

    Update Searchbar.styled.jsx

--- src/components/Searchbar/Searchbar.styled.jsx
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -17,7 +17,7 @@ export const Header = styled.header`
   padding-top: ${props => props.theme.space[3]}px;
   padding-bottom: ${props => props.theme.space[3]}px;
 
-  color: #fff;
+  color: ${props => props.theme.colors.white};
   background-color: ${props => props.theme.colors.secondary};
   box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
     0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);

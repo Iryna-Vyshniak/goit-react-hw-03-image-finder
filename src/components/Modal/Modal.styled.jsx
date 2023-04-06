39ea41fe4db3dcb1dccb6824079dcb0eeb1d577f
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-06 14:10:46 +0300

    Update Modal.styled.jsx

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -14,11 +14,14 @@ export const ModalContent = styled.div`
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  min-height: 300px;
-  max-width: 600px;
+
+  min-height: 350px;
+  max-width: 60vw;
   width: 100%;
+
   padding: 12px;
-  background-color: #fff;
+  background-color: #fffbe6;
+
   border-radius: 3px;
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);

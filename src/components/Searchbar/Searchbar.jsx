b76878b4eb5ee5f0d589dfe21386246ca8513763
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-07 20:56:40 +0300

    add state, handleChange and handleSubmit

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {
   Header,
   SearchForm,
@@ -8,20 +8,40 @@ import {
 import { HiMagnifyingGlass } from 'react-icons/hi2';
 //import PropTypes from 'prop-types';
 
-export const Searchbar = () => {
-  return (
-    <Header>
-      <SearchForm>
-        <SearchFormBtn>
-          <HiMagnifyingGlass size="24" />
-        </SearchFormBtn>
-        <SearchFormInput
-          type="text"
-          autocomplete="off"
-          autoFocus
-          placeholder="Search images and photos"
-        />
-      </SearchForm>
-    </Header>
-  );
-};
+export class Searchbar extends Component {
+  state = {
+    value: '',
+  };
+
+  handleChange = ({ target: { value } }) => {
+    this.setState({ value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSearch(this.state.value);
+    this.setState({ value: '' });
+  };
+
+  render() {
+    const { value } = this.state;
+
+    return (
+      <Header>
+        <SearchForm onSubmit={this.handleSubmit}>
+          <SearchFormBtn>
+            <HiMagnifyingGlass size="24" />
+          </SearchFormBtn>
+          <SearchFormInput
+            type="text"
+            autocomplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={value}
+            onChange={this.handleChange}
+          />
+        </SearchForm>
+      </Header>
+    );
+  }
+}

2d79e1974f058d727a18945a430b86f1c09e5904
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-07 20:57:09 +0300

    add state textSearch, handleSubmit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,13 @@ import Modal from './Modal/Modal';
 import { InitialStateGallery } from './InitialStateGallery/InitialStateGallery';
 import DefaultImg from 'assets/pbsh.png';
 import { ModalDescr, ModalImg } from './App.styled';
+import { Searchbar } from './Searchbar/Searchbar';
+//import { Loader } from './Loader/Loader';
 
 export default class App extends Component {
   state = {
+    textSearch: '',
+    isLoading: false,
     isShowModal: false,
     largeImgUrl: DefaultImg,
     imgTags: 'Oops... there are no images matching your search',
@@ -18,31 +22,40 @@ export default class App extends Component {
   componentWillUnmount() {}
 
   // custom methods
+  handleSubmit = textSearch => {
+    this.setState({ textSearch });
+  };
 
   // for toggle modal
   toggleModal = () => {
-    this.setState(({ isShowModal }) => ({ isShowModal: !isShowModal }));
+    this.setState(({ isShowModal }) => ({
+      isShowModal: !isShowModal,
+    }));
   };
 
   render() {
     const { isShowModal, largeImgUrl, imgTags } = this.state;
 
+    console.log('state:', this.state);
     return (
-      <Layout>
-        {/* test button */}
-        <button type="button" onClick={this.toggleModal}>
-          Test Open Modal
-        </button>
-        <InitialStateGallery text="Let`s find images together!" />
-        {/* render modal by condition */}
-        {isShowModal && (
-          <Modal onClose={this.toggleModal}>
-            <ModalImg src={largeImgUrl} alt="" />
-            <ModalDescr>{imgTags}</ModalDescr>
-          </Modal>
-        )}
+      <>
+        <Searchbar onSearch={this.handleSubmit} />
+        <Layout>
+          <InitialStateGallery text="Let`s find images together!" />
+
+          {/* render Loading by condition */}
+
+          {/* render modal by condition */}
+
+          {isShowModal && (
+            <Modal onClose={this.toggleModal}>
+              <ModalImg src={largeImgUrl} alt="" />
+              <ModalDescr>{imgTags}</ModalDescr>
+            </Modal>
+          )}
+        </Layout>
         <GlobalStyle />
-      </Layout>
+      </>
     );
   }
 }

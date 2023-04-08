0f0e842c1ccc96f150360b4b43b13e8d5fd29802
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-08 22:52:23 +0300

    Update Modal.styled.jsx

--- src/components/Modal/Modal.styled.jsx
+++ src/components/Modal/Modal.styled.jsx
@@ -6,7 +6,7 @@ export const ModalBackdrop = styled.div`
   left: 0;
   width: 100vw;
   height: 100vh;
-  background-color: rgba(185, 228, 201, 0.2);
+  background-color: rgba(0, 0, 0, 0.7);
 `;
 
 export const ModalContent = styled.div`
@@ -15,14 +15,47 @@ export const ModalContent = styled.div`
   left: 50%;
   transform: translate(-50%, -50%);
 
-  min-height: 350px;
-  max-width: 60vw;
-  width: 100%;
+  padding-right: ${props => props.theme.space[4]}px;
+  padding-left: ${props => props.theme.space[4]}px;
+  padding-top: ${props => props.theme.space[4]}px;
+  padding-bottom: ${props => props.theme.space[4]}px;
 
-  padding: 12px;
+  max-width: calc(100vw - 48px);
+  max-height: calc(100vh - 24px);
+
   background-color: #fffbe6;
 
   border-radius: 3px;
   box-shadow: 0px 2px 1px -1px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 1px 3px 0px rgba(0, 0, 0, 0.12);
+`;
+
+export const ModalDescr = styled.p`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  margin: 0 auto;
+  padding-right: ${props => props.theme.space[4]}px;
+  padding-left: ${props => props.theme.space[4]}px;
+  padding-top: ${props => props.theme.space[4]}px;
+  padding-bottom: ${props => props.theme.space[4]}px;
+
+  width: 100%;
+
+  color: ${props => props.theme.colors.accent};
+  text-shadow: ${props => props.theme.shadows.textShadow};
+  background-color: rgba(185, 228, 201, 0.5);
+  box-shadow: 0px -2px 4px 1px rgba(0, 0, 0, 0.2),
+    0px -4px 5px 0px rgba(0, 0, 0, 0.14), 0px -1px 10px 0px rgba(0, 0, 0, 0.12);
+
+  backdrop-filter: blur(5.5px);
+
+  font-family: ${props => props.theme.fonts.heading}
+  font-size: ${props => props.theme.fontSizes.l};
+  text-align: center;
 `;

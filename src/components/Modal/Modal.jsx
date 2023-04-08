99117004b53f76b2988540ad3b2f379ef2a33aaf
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-08 22:55:30 +0300

    add propTypes, props

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,19 @@
-//import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { createPortal } from 'react-dom';
-import { ModalBackdrop, ModalContent } from './Modal.styled';
+import { ModalBackdrop, ModalContent, ModalDescr } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
 export default class Modal extends Component {
+  static propTypes = {
+    modalData: PropTypes.shape({
+      largeImageURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+    }),
+    onModalClose: PropTypes.func,
+  };
+
   componentDidMount() {
     window.addEventListener('keydown', this.handleKeyDown);
   }
@@ -16,20 +24,25 @@ export default class Modal extends Component {
 
   handleKeyDown = e => {
     if (e.code === `Escape`) {
-      this.props.onClose();
+      this.props.onModalClose();
     }
   };
 
   handleBackdropeClick = e => {
     if (e.target === e.currentTarget) {
-      this.props.onClose();
+      this.props.onModalClose();
     }
   };
 
   render() {
+    const { largeImageURL, tags } = this.props.modalData;
+
     return createPortal(
       <ModalBackdrop onClick={this.handleBackdropeClick}>
-        <ModalContent>{this.props.children}</ModalContent>
+        <ModalContent>
+          <img src={largeImageURL} alt={tags} />
+          <ModalDescr>{tags}</ModalDescr>
+        </ModalContent>
       </ModalBackdrop>,
       modalRoot
     );

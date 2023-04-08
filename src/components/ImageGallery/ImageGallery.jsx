4656a423b1609a6fdaf369d8a12430e15b7c7c16
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-08 19:14:25 +0300

    add load more btn

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,12 +3,13 @@ import imagesAPI from 'services/getImages';
 import React, { Component } from 'react';
 import { List } from './ImageGallery.styled';
 import { ImageGalleryItem } from '../ImageGalleryItem/ImageGalleryItem';
-import DefaultImg from 'assets/pbsh.png';
-import { toast } from 'react-toastify';
-import { notifyOptions } from '../notify/notify';
+//import DefaultImg from 'assets/pbsh.png';
+// import { toast } from 'react-toastify';
+// import { notifyOptions } from '../notify/notify';
 import { Loader } from '../Loader/Loader';
 import ImageErrorView from 'components/ImageErrorView/ImageErrorView';
 import { InitialStateGallery } from '../InitialStateGallery/InitialStateGallery';
+import { Button } from 'components/Button/Button';
 
 const Status = {
   IDLE: 'idle',
@@ -19,29 +20,40 @@ const Status = {
 
 export default class ImageGallery extends Component {
   state = {
-    images: null,
+    value: '',
+    images: [],
     error: null,
     status: Status.IDLE,
-
-    largeImgUrl: DefaultImg,
+    page: 1,
   };
 
-  componentDidUpdate(prevProps) {
+  // перевіряємо, щоб в пропсах змінився запит
+  // y static відсутній this, тому дублюємо в state - search: ''
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.value !== nextProps.value) {
+      return { page: 1, value: nextProps.value, images: [] };
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { page } = this.state;
     const prevValue = prevProps.value;
     const nextValue = this.props.value;
     //console.log(nextValue);
     //   страхуємо від повторного запиту, якщо вже таке слово було введене
-    if (prevValue !== nextValue) {
+    if (prevValue !== nextValue || prevState.page !== page) {
       // console.log('prevValue:', prevValue);
       //console.log('nextValue:', nextValue);
       this.setState({ status: Status.PENDING });
 
       imagesAPI
-        .getImages(nextValue)
-        .then(images => {
-          //console.log(images);
+        .getImages(nextValue, page)
+        .then(({ hits }) => {
+          //console.log(hits);
+
           this.setState({
-            images,
+            images: [...this.state.images, ...hits],
             status: Status.RESOLVED,
           });
           // console.log(this.state.images);
@@ -49,6 +61,11 @@ export default class ImageGallery extends Component {
         .catch(error => this.setState({ error, status: Status.REJECTED }));
     }
   }
+
+  // custom methods
+  handleLoad = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
 
   render() {
     const { images, error, status } = this.state;
@@ -64,7 +81,7 @@ export default class ImageGallery extends Component {
     if (status === 'rejected') {
       return <ImageErrorView message={error.message} />;
     }
-    if (images.hits.length === 0) {
+    if (images.length === 0) {
       return (
         <ImageErrorView
           message={`Oops... there are no images matching your search... `}
@@ -72,42 +89,20 @@ export default class ImageGallery extends Component {
       );
     }
     if (status === 'resolved') {
-      toast.success(`Hooray! We found ${images.total} images.`, notifyOptions);
+      //toast.success(`Hooray! We found ${images.total} images.`, notifyOptions);
       return (
-        <List>
-          {images.hits.map(image => (
-            <ImageGalleryItem
-              key={image.id}
-              item={image}
-              //   showModal={this.toggleModal}
-            />
-          ))}
-        </List>
-      );
-    }
-    /* return (
-      <>
-        {error && (
-          <>
-            <ImageErrorView />
-            <p>{error.message}</p>
-          </>
-        )}
-
-        {!value && <InitialStateGallery text="Let`s find images together!" />}
-        {isLoading && <Loader loading={isLoading} />}
-        {images && (
+        <>
           <List>
-            {images.hits.map(image => (
-              <ImageGalleryItem
-                key={image.id}
-                item={image}
-              
-              />
+            {images.map(image => (
+              <ImageGalleryItem key={image.id} item={image} />
             ))}
           </List>
-        )}
-      </>
-    ); */
+
+          <Button status="load" onClick={this.handleLoad}>
+            Load More
+          </Button>
+        </>
+      );
+    }
   }
 }

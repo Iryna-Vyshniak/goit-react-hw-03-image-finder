72eb16cdfadabbcb284b8cc5fd6583860ec8bb09
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-04-08 15:47:13 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,21 +1,36 @@
-import { RevolvingDot } from 'react-loader-spinner';
+//import { RevolvingDot } from 'react-loader-spinner';
 import React from 'react';
-import { LoaderBackdrop } from './Loader.styled';
+import { LoaderBackdrop, PendingDog } from './Loader.styled';
+import Dog from 'assets/pending.png';
+import DotLoader from 'react-spinners/DotLoader';
 
 export const Loader = () => {
   return (
     <LoaderBackdrop>
-      <RevolvingDot
-        height="150"
-        width="150"
-        radius="50"
-        color="#FD5523"
-        secondaryColor="#37966F"
-        ariaLabel="revolving-dot-loading"
-        wrapperStyle={{ position: 'absolute', top: '45%', left: '45%' }}
-        wrapperClass="revolving-dot-wrapper"
-        visible={true}
+      <DotLoader
+        color="#f68000"
+        size={150}
+        cssOverride={{ position: 'absolute', top: '25%', left: '45%' }}
       />
+      <PendingDog src={Dog} alt="Dog" />
     </LoaderBackdrop>
   );
 };
+// export const Loader = () => {
+//   return (
+//     <LoaderBackdrop>
+//       <RevolvingDot
+//         height="150"
+//         width="150"
+//         radius="50"
+//         color="#FD5523"
+//         secondaryColor="#37966F"
+//         ariaLabel="revolving-dot-loading"
+//         wrapperStyle={{ position: 'absolute', top: '45%', left: '45%' }}
+//         wrapperClass="revolving-dot-wrapper"
+//         visible={true}
+//       />
+//       <PendingDog src={Dog} alt="Dog" />
+//     </LoaderBackdrop>
+//   );
+// };
